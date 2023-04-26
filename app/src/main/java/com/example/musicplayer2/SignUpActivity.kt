cf48d37eacfaa21fcfb15a75fced3d62391e370d
Shivangi <shivangit1111@gmail.com>
2023-04-26 16:07:08 +0530

    chore: make variables

--- app/src/main/java/com/example/musicplayer2/SignUpActivity.kt
+++ app/src/main/java/com/example/musicplayer2/SignUpActivity.kt
@@ -2,11 +2,15 @@ package com.example.musicplayer2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.musicplayer2.databinding.ActivitySignUpBinding
+import com.google.firebase.database.DatabaseReference
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
+    lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,5 +18,17 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        binding.materialButton.setOnClickListener {
+            val name = binding.etName.text.toString()
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassward.text.toString()
+            val uniqueId = binding.etUniqeId.text.toString()
+
+
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty() || uniqueId.isEmpty()) {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
