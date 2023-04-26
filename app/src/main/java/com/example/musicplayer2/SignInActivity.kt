70af4e11d5949c43d2cae83cac45198fb598ce63
Shivangi <shivangit1111@gmail.com>
2023-04-26 15:17:22 +0530

    feat: implement binding

--- app/src/main/java/com/example/musicplayer2/SignInActivity.kt
+++ app/src/main/java/com/example/musicplayer2/SignInActivity.kt
@@ -2,10 +2,15 @@ package com.example.musicplayer2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.musicplayer2.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
