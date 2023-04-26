af78e0c78fda54453b2815a6f5daf1324734c8af
Shivangi <shivangit1111@gmail.com>
2023-04-26 14:55:26 +0530

    feat: make splash screen

--- app/src/main/java/com/example/musicplayer2/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer2/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.musicplayer2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.musicplayer2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
+        Handler().postDelayed({
+
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
+
 }
